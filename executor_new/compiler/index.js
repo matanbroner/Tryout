e012cc11648cd9eb2f4aa514130c08c7c111a004
Matan Broner <matanbroner@gmail.com>
2020-10-26 22:45:43 -0700

    JS doc for compiler index

--- executor_new/compiler/index.js
+++ executor_new/compiler/index.js
@@ -1,15 +1,19 @@
 const fs = require("fs").promises;
 const path = require("path");
-const uril = require("./util");
-const { CompilerExecution } = require("./core_objects");
 const util = require("./util");
+const { CompilerExecution } = require("./core_objects");
 
 class Compiler {
+  /**
+   * Initiates build and execution of given code
+   * @param  {String} code
+   * @param  {String} language
+   * @param  {Function} callback - function accepting stdout and stderr
+   * @param  {Object} settings
+   * @returns {CompilerExecution} - timed execution of the given code
+   */
   async runCode(code, language, callback, settings = {}) {
-    const {
-      fileId,
-      filePath
-    } = await this.generateBuildFile(code, language);
+    const { fileId, filePath } = await this.generateBuildFile(code, language);
     const modCallback = this.generateExecutionCallback(
       filePath,
       language,
@@ -19,6 +23,12 @@ class Compiler {
     return new CompilerExecution(filePath, language, modCallback, settings);
   }
 
+  /**
+   * Generates a language dependent file with inputed code
+   * @param  {String} code
+   * @param  {String} language
+   * @returns {Object} ID and path of generated code file
+   */
   async generateBuildFile(code, language) {
     let fileId = util.all.uuid();
     const modifiedBuild = this.formatPreBuild(language, code, fileId);
@@ -35,12 +45,20 @@ class Compiler {
     }
     return {
       fileId,
-      filePath
+      filePath,
     };
   }
 
+  /**
+   * Creates a decorated parent callback which handles compiler related tasks on execution complete
+   * @param  {String} filePath
+   * @param  {String} language
+   * @param  {String} fileId
+   * @param  {Function} callback
+   * @returns {Function} modified callback
+   */
   generateExecutionCallback(filePath, language, fileId, callback) {
-    const execCallback = function(output, errors){
+    const execCallback = function (output, errors) {
       fs.unlink(filePath);
       this.cleanPostBuild(language)(output, errors, fileId);
       callback(output, errors);
@@ -48,6 +66,13 @@ class Compiler {
     return execCallback;
   }
 
+  /**
+   * Applies language specific preformatting of code and build file
+   * @param  {String} language
+   * @param  {String} code
+   * @param  {String} fileId
+   * @returns {Object} modified code and file ID
+   */
   formatPreBuild(language, code, fileId) {
     switch (language) {
       case "java":
@@ -60,15 +85,27 @@ class Compiler {
     }
   }
 
+  /**
+   * Applies language specific cleanup post execution complete
+   * @param  {String} language
+   * @returns {Function} cleanup utility function
+   */
   cleanPostBuild(language) {
     switch (language) {
       case "java":
         return util.java.cleanPostBuild;
       default:
-        return function() { return };
+        return function () {
+          return;
+        };
     }
   }
 
+  /**
+   * Returns correct file suffix based on language
+   * @param  {String} language
+   * @returns {String} file suffix
+   */
   fileSuffix(language) {
     switch (language) {
       case "python":
